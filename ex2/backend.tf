cbb846fe0c12b4b02512c132a49239366fac7244
Sushma <sushma.gayatri451@gmail.com>
2022-11-24 17:46:59 +0530

    ec2

---
+++ ex2/backend.tf
@@ -0,0 +1,11 @@
+terraform {
+    backend "s3" {
+        bucket     = "bsg-tf-state-bucket"
+        key        = "module/env/terraform.tfstate"
+        region     = "us-east-1"
+    }
+}
+
+provider "aws" {
+    region = "us-ease-1"
+}
---
+++ ex2/ec2/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "demo1" {
+    ami                     = "ami-00e87074e52e6c9f9"
+    instance_type           = "t3.micro"
+    vpc_security_group_ids = [var.sg]
+
+    tags = {
+        Name = "EC2-terraform"
+    }
+
+variable "sg" {}
