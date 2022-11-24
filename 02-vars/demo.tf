e72085748756c834b767061e9c406fe77c0cc2d7
Sushma <sushma.gayatri451@gmail.com>
2022-11-24 11:22:49 +0530

    Basics

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -6,3 +6,9 @@ output "sample-op" {
     value = var.sample
 }
 
+output "sample-op1" {
+    value = "Value is ${var.sample}"
+}
+
+# A variable can be accessed with our without ${} only if the variable is called. BUt in case if it is needed to add with some other string then we need to use double quotes.
+# There is no concept of single quotes in terraform.
