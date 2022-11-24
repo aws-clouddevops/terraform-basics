f2333376106cdea1dc28ad42b5936c933518a4ad
Sushma <sushma.gayatri451@gmail.com>
2022-11-24 18:49:24 +0530

    ami

--- ex2/ec2/data.tf
+++ ex2/ec2/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "my_ami" {
   most_recent      = true
   name_regex       = "DevOps-LabImage-CentOS7"
-  owners           = ["amazon"]
+  owners           = ["aws-marketplace"]
 
 }
 
