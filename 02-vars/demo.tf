358de62b805bad38fa51858f2fa1aa1c4155992f
Sushma <sushma.gayatri451@gmail.com>
2022-11-24 11:44:12 +0530

    basics

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -12,3 +12,25 @@ output "sample-op1" {
 
 # A variable can be accessed with our without ${} only if the variable is called. BUt in case if it is needed to add with some other string then we need to use double quotes.
 # There is no concept of single quotes in terraform.
+
+variable "numbers" {
+    default =100
+}
+
+variable "datatypes" {
+    default = [
+        "Sample" ,
+        500 ,
+        true ,
+        "AWS",
+        "Azure"
+    ]
+}
+
+variable "types" {
+    default = {
+        list = "Good Morning",
+        number = 200,
+        boolean = true
+    }
+}
