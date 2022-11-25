86f59529dbae2c22e8dacb98bff55c2176b71323
Sushma <sushma.gayatri451@gmail.com>
2022-11-25 13:40:57 +0530

    ec2

--- project-setup/ec2.tf
+++ project-setup/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "demo1" {
+resource "aws_spot_instance_request" "cheap_worker"{
     ami                     = "data.aws_ami.my_ami.id"
     instance_type           = "t3.micro"
     vpc_security_group_ids = [var.sg]
@@ -13,7 +13,7 @@ resource "aws_instance" "demo1" {
     type     = "ssh"
     user     = "centos"
     password = "DevOps321"
-    host     = self.public_ip
+    host     = self.private_ip
   }
 
     inline = [
