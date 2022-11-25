7070eea4d72e7508ec31fd4333ff070e3db2fc81
Sushma <sushma.gayatri451@gmail.com>
2022-11-25 16:43:05 +0530

    ec2

--- project-setup/ec2/ec2.tf
+++ project-setup/ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_instance_request" "spot_worker" {
-    ami                     = "data.aws_ami.my_ami.id"
+    ami                     = data.aws_ami.my_ami.id
     instance_type           = "t3.micro"
     wait_for_fulfillment    = true
     vpc_security_group_ids = [aws_security_group.allow_ssh.id]
